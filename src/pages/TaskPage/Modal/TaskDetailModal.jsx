e68b056815b5732413e7c31e54be619cc6b9aa50
thuynguyentvt333 <thuynguyen2707yp@gmail.com>
2024-08-20 20:56:39 +0700

    update modal view detail a task

--- src/pages/TaskPage/Modal/TaskDetailModal.jsx
+++ src/pages/TaskPage/Modal/TaskDetailModal.jsx
@@ -11,10 +11,7 @@ const TaskDetailModal = ({ isOpen, toggle, task }) => {
                     <div>
                         <p><strong>Task Name:</strong> {task.title}</p>
                         <p><strong>Description:</strong> {task.description}</p>
-                        <p><strong>Status:</strong> {task.status_id}</p>
                         <p><strong>Priority:</strong> {task.priority_id === 1 ? 'High' : task.priority_id === 2 ? 'Medium' : 'Low'}</p>
-                        <p><strong>Group:</strong> {task.group_id ? `Group ${task.group_id}` : 'No Group'}</p>
-                        <p><strong>Parent Task:</strong> {task.parent_id ? `Task ${task.parent_id}` : 'No Parent'}</p>
                         <p><strong>Start Date:</strong> {task.start_date}</p>
                         <p><strong>End Date:</strong> {task.end_date}</p>
                         <p><strong>Start Time:</strong> {task.start_time}</p>
